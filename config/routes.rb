e8f9e595df12a4e0d9a21d60971a28af0a37af2f
Matthew Loseke <mloseke@gmail.com>
2013-03-20 16:07:28 -0700

    set up subscriptions and splash page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Trpn::Application.routes.draw do
+  get "splash/index"
+
+  resources :subscriptions
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +53,7 @@ Trpn::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'splash#index'
 
   # See how all your routes lay out with "rake routes"
 
