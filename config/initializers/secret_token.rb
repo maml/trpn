93bfef0ac395e709fb396c7f23e191ad8416ffed
Matthew Loseke <mloseke@gmail.com>
2013-03-20 13:29:18 -0700

    initialize rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trpn::Application.config.secret_token = '989481453879d0941f49570655d1498029ca27aa0f1c352ba95d2af79217d186996c5158d5b8f7720109daa4e03c8ee6d2aa4f77ebd16f44242371e883e85210'
