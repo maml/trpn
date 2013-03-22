73ca580e2623362555a31e36d0d904d0829fd6f4
Matthew Loseke <mloseke@gmail.com>
2013-03-21 19:53:24 -0700

    delete cruft from production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,18 +54,10 @@ Trpn::Application.configure do
 	config.action_mailer.smtp_settings = {
 		:address              => "smtp.mandrillapp.com",
 		:port                 => 25,
-		#:domain               => 'trpn.herokuapp.com',
 		:user_name            => "#{$MANDRILL_USERNAME}",
 		:password             => "#{$MANDRILL_APIKEY}",
 		:authentication       => 'login',
 		:enable_starttls_auto => true  }
-
-:address   => "smtp.mandrillapp.com",
-    :port      => 25, # ports 587 and 2525 are also supported with STARTTLS
-    :enable_starttls_auto => true, # detects and uses STARTTLS
-    :user_name => "MANDRILL_USERNAME",
-    :password  => "MANDRILL_PASSWORD", # SMTP password is any valid API key
-    :authentication => 'login' # Mandrill supports 'plain' or 'login'
 
   # Enable threaded mode
   # config.threadsafe!
