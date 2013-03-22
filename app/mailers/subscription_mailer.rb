50a7da714f31ffbbe03eae2631dd2cb82665d06b
Matthew Loseke <mloseke@gmail.com>
2013-03-21 19:16:27 -0700

    only send launch notification email in production

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -2,8 +2,10 @@ class SubscriptionMailer < ActionMailer::Base
   default from: "notifications@trpn.me"
 
 	def launch_notification_email(subscription)
-		@subscription = subscription
-		@url = "http://trpn.me"
-		mail(:to => subscription.email, :subject => "Welcome to TRPN's launch notification list!")
+		if $RACK_ENV == 'production'
+			@subscription = subscription
+			@url = "http://trpn.me"
+			mail(:to => subscription.email, :subject => "Welcome to TRPN's launch notification list!")
+		end
 	end
 end
