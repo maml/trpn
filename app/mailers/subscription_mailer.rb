612ace0cc727aa33d3a9e4426ff31a660891cbee
Matthew Loseke <mloseke@gmail.com>
2013-03-22 11:15:42 -0700

    add some puts to launch_notification_email

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -2,7 +2,10 @@ class SubscriptionMailer < ActionMailer::Base
   default from: "notifications@trpn.me"
 
 	def launch_notification_email(subscription)
+		puts "!!!\nlaunch_notification_email called\n!!!"
+		puts "subscription: #{subscription.inspect}"
 		if $RACK_ENV == 'production'
+			puts "$RACK_ENV is production"
 			@subscription = subscription
 			@url = "http://trpn.me"
 			mail(:to => subscription.email, :subject => "Welcome to TRPN's launch notification list!")
