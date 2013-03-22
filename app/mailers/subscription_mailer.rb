f891c89b1f66c06c322e9fd2bfeb1abef99ce036
Matthew Loseke <mloseke@gmail.com>
2013-03-22 11:19:59 -0700

    inspect  in launch_notification_email

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -4,8 +4,9 @@ class SubscriptionMailer < ActionMailer::Base
 	def launch_notification_email(subscription)
 		puts "!!!\nlaunch_notification_email called\n!!!"
 		puts "subscription: #{subscription.inspect}"
+		puts "$RACK_ENV is #{$RACK_ENV}"
+		puts "$RACK_ENV is #{$RACK_ENV.inspect}"
 		if $RACK_ENV == 'production'
-			puts "$RACK_ENV is production"
 			@subscription = subscription
 			@url = "http://trpn.me"
 			mail(:to => subscription.email, :subject => "Welcome to TRPN's launch notification list!")
