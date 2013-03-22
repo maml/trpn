6cf7cc85147632f17abaea43a00d6c8722a92074
Matthew Loseke <mloseke@gmail.com>
2013-03-22 14:02:06 -0700

    send launch notification email unless we're in development environment

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -2,7 +2,7 @@ class SubscriptionMailer < ActionMailer::Base
   default from: "notifications@trpn.me"
 
 	def launch_notification_email(subscription)
-		if Rails.env == "production"
+		unless Rails.env == "development"
 			@subscription = subscription
 			@url = "http://trpn.me"
 			mail(:to => subscription.email, :subject => "Welcome to TRPN's launch notification list!")
