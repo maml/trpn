f48486cd0d36ab63a4450c97c29c4e40cb0fc65c
Matthew Loseke <mloseke@gmail.com>
2013-03-22 11:26:20 -0700

    remove  if statement from launch_notification_email

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -4,12 +4,8 @@ class SubscriptionMailer < ActionMailer::Base
 	def launch_notification_email(subscription)
 		puts "!!!\nlaunch_notification_email called\n!!!"
 		puts "subscription: #{subscription.inspect}"
-		puts "$RACK_ENV is #{$RACK_ENV}"
-		puts "$RACK_ENV is #{$RACK_ENV.inspect}"
-		if $RACK_ENV == 'production'
-			@subscription = subscription
-			@url = "http://trpn.me"
-			mail(:to => subscription.email, :subject => "Welcome to TRPN's launch notification list!")
-		end
+		@subscription = subscription
+		@url = "http://trpn.me"
+		mail(:to => subscription.email, :subject => "Welcome to TRPN's launch notification list!")
 	end
 end
