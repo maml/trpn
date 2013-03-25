997e8eef20e2884c33a3aaf13fe878e696ff88f2
Matthew Loseke <mloseke@gmail.com>
2013-03-25 15:46:42 -0500

    password encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
+	attr_accessor :salt, :encrypted_password
 
 	has_many :rides
+
+	before_save :encrypt_password
+	after_save  :clear_password
 	
 	# format check for email based on http://davidcel.is/blog/2012/09/06/stop-validating-email-addresses-with-regex/
 	validates :email,
@@ -19,4 +23,16 @@ class User < ActiveRecord::Base
 		}
 	validates :password_confirmation,
 		:presence 		=> true
+
+	def encrypt_password
+		if password.present?
+			self.salt = BCrypt::Engine.generate_salt
+			self.encrypted_password= BCrypt::Engine.hash_secret(password, salt)
+			self.password = self.encrypted_password
+		end
+	end
+
+	def clear_password
+		self.password = nil
+	end
 end
