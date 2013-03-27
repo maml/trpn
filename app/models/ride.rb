9716cba2c3eb1aa2baf53401595ce468ecfe0f88
Matthew Loseke <mloseke@gmail.com>
2013-03-27 18:56:56 -0500

    remove latitude and longitude from attr_accessible in Ride

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,7 +1,7 @@
 class Ride < ActiveRecord::Base
 	self.rgeo_factory_generator = RGeo::Geos.factory_generator(:srid => 4326)
 
-  attr_accessible :description, :latitude, :longitude, :request, :title, :center
+  attr_accessible :description, :request, :title, :center
 
 	validates :title, :presence => true
 
