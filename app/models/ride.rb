9dae7ed4597d3380e3deb765cf0fd6680d459bd8
Matthew Loseke <mloseke@gmail.com>
2013-03-27 18:18:42 -0500

    add delegations and validations for latitude and longitude on Ride  - latitude delegates to center (previously aliassed to y)  - longitude delegates to center (previously aliased to x)  - latitude validates presence & that it falls between -90 and 90  - longitude validates presence & that it falls between -180 and 180  - also update seeds for correct center points

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -4,4 +4,21 @@ class Ride < ActiveRecord::Base
   attr_accessible :description, :latitude, :longitude, :request, :title, :center
 
 	validates :title, :presence => true
+
+	delegate :latitude, :to => :center, :allow_nil => true
+  delegate :longitude, :to => :center, :allow_nil => true
+
+	validates :latitude,
+		:presence => true,
+		:numericality => {
+			:greater_than_or_equal_to => -90,
+			:less_than_or_equal_to => 90
+		}
+	
+  validates :longitude,
+		:presence => true,
+		:numericality => {
+			:greater_than_or_equal_to => -180,
+			:less_than_or_equal_to => 180
+		}
 end
