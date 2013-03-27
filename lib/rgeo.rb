6c602b00ba5d2c1e8b611c869cc237071ff446b6
Matthew Loseke <mloseke@gmail.com>
2013-03-27 18:56:10 -0500

    remove unused aliases from rgeo monkey patch

--- lib/rgeo.rb
+++ lib/rgeo.rb
@@ -1,12 +1,4 @@
 class RGeo::Geos::CAPIPointImpl
-
-	# only using latitude and longitude, could probably remove lat, lon, and lng
-
-  alias_method :lat, :y
-  alias_method :latitude, :y
-
-  alias_method :lon, :x
-  alias_method :lng, :x
+	alias_method :latitude, :y
   alias_method :longitude, :x
-
 end
