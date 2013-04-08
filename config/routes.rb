37c86bafcbfe28ce11e4c01e3e0eee949b9b50d9
Matthew Loseke <mloseke@gmail.com>
2013-04-08 18:22:17 -0500

    return json by default for /users

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Trpn::Application.routes.draw do
 	get "splash/index"
 	root :to => 'splash#index'
   
-	resources :users do
+	resources :users, :defaults => { :format => 'json' } do
   	resources :rides
 	end
-
+	
 	resources :subscriptions, :only => [:create]
 
 end
