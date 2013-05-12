d52ba2134c5a7d1cd5549ac868b19357b1fa9263
Matthew Loseke <mloseke@gmail.com>
2013-05-11 19:25:35 -0500

    Ride no longer requires presence of a title

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,8 +1,6 @@
 class Ride < ActiveRecord::Base
   attr_accessible :description, :latitude, :longitude, :request, :title, :address, :to, :from, :date
 
-	validates :title, :presence => true
-
 	reverse_geocoded_by :latitude, :longitude
 	after_validation :reverse_geocode, :if => :latitude_changed? || :longitude_changed?
 
--- test/unit/ride_test.rb
+++ test/unit/ride_test.rb
@@ -1,12 +1,6 @@
 require 'test_helper'
 
 class RideTest < ActiveSupport::TestCase
-  test "it doesn't save a ride without a title" do
-  	ride = rides(:one)
-		ride.title = nil
-	 	assert !ride.save, 'Saved a ride without a title'
-  end
-
 	test "it shows rides nearby" do
 		ride = rides(:one)
 		assert ride.nearby.includes(rides(:two))
