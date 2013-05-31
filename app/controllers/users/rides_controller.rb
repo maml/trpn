6062d1e713f2613b031f3ad71f7f58a1da489ecc
Matthew Loseke <mloseke@gmail.com>
2013-05-30 22:10:48 -0500

    remove users/rides_controller's show method, rides_controller's show handles everything fine

--- app/controllers/users/rides_controller.rb
+++ app/controllers/users/rides_controller.rb
@@ -9,13 +9,6 @@ class Users::RidesController < RidesController
 		super
 	end
 
-	# GET /users/:id/rides/1
-	# GET /users/:id/rides/1.json
-	def show
-		@ride = @user.rides.find(params[:id])
-		super
-	end
-	
 	# GET /users/:id/rides/new
 	# GET /users/:id/rides/new.json
 	def new
