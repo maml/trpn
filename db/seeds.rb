e8fea4443e14b23d134ed049e5227c08b16b90dd
Matthew Loseke <mloseke@gmail.com>
2013-06-09 22:06:23 -0500

    update seed data, remove debugger instance

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,63 +14,56 @@ Ride.delete_all
 # Wed, Thu, Fri, Sat, Sun, Mon, Tue
 
 @bstolte_rides = Ride.create([
+				{ request: false,
+					title: "Ride offer Chicago to Toronto to Montreal",
+					description: "Hey everyone! We are a French couple traveling in our van across the country. We are going east. We are now in Winnipeg and going to Montreal through Chicago and Toronto. We have 2 extra seats and some room for your stuff. If anybody want to join us for a fun roadtrip, let us know!!",
+					from: 'Chicago, IL',
+				},
+				{ request: false,
+					title: "ride to bonnoroo!! i drive 6/10",
+					description: "I am going to volunteer at bonnoroo, which is in Tennessee. I want to be on my own slack schedule so that is why I will be driving my car, a hatchback Volkswagen rabbit. I am willing to carpool with people who are going there, or give people a ride along the way. I may even visit a friend in ashville, NC. So I would not mind much detouring, especially if you have some interesting sights to show me. Email me for more details or my number",
+					from: 'Lemont, IL',
+				},
 				{ request: true,
-					description: 'Going surfing with my friend. Willing to split gas money.',
-					from: 'San Francisco, CA',
-					to: 'Long Beach, CA',
-					date: 'Wed, 29 May 2013'
-				},
-				{ request: false,
-					description: "Heading back from my aunt's house. You have to buy me lunch and dinner.",
-					from: 'Portland, OR',
-					to: 'San Francisco, CA',
-					date: 'Thu, 30 May 2013'
-				},
-				{ request: false,
-					description: "I am offering a ride from Berkeley (SF Bay Area) to Portland OR, early AM on 03-25-2013. Luggage is welcome, door-to-door. No smokers, dopers, dogs, pets, or bicycles. If you are interested in a ride, please email me your telephone number and physical street address, so that I know how to find you. If this ad is still up, that means there is room in the car, and that I haven't left yet. Asking $62/person, or $60 if you are able to be dropped off on my route.",
-					from: 'San Francisco, CA',
-					to: 'Portland, OR',
-					date: 'Fri, 31 May 2013'
+					title: "could really use some help (ride) chicago to nj",
+					description: "Hello,\n\nI am in a really tough situation and could really use some help from a friend. My former friend ditched me here in galesburge, IL after a disagreement, with no money, no cell phone, no way to contact anyone I know back home, and worst of all, I am 1000 miles away from home and could really use someone. If you are going east and need someone to drive with or could give me a ride that would be great! or if you have a spare bed for the night so I could have enough time to get money from home that would me amazing! Even if you could spare a dollar or a phone call I would forever be indepted to you.\n\nI am a 22 year old college student, and I have no idea what to freaking do..\n\nAny help would be so much appreciated! ( hopefully there is some humanity still left out there)\n\nPlease email me",
+					from: "Galesburg, IL",
 				}])
 
 @keithnorm_rides = Ride.create([
-				{ request: true,
-					description: 'Want to hunt down an horchata latte. Willing to split gas money.',
-					from: 'San Francisco, CA',
-					to: 'Los Angeles, CA',
-					date: 'Sat, 1 June 2013'
+				{ request: false,
+					title: "Ride offered, Chicago -> Portland, OR, 6/10",
+					description: "heading out early morning 6/10, 4 day journey. email if interested/looking for details.
+looking for a passenger to ride part of or the whole way there. must have a driver's license, be clean, and no sketchy drugs.",
+					from: "Chicago, IL",
+					date: '10 June 2013'
+				},
+				{ request: false,
+					title: "bonnaroo 2013",
+					description: "I am leaving monday or tuesday for bonnaroo in manchester TN, anyone wanna ride and share gas cost let me know. I also have an extra pass if you need one.",
+					from: "Chicago, IL",
 				},
 				{ request: true,
-					description: 'Hey! A friend and i desperately need to get back to the nw, specifically olympia wa, but anywhere close works. We have $100 between the two of us but can maybe conjure up more. Email or call 828777two719 if you can help us out!',
-					from: 'San Francisco, CA',
-					to: 'Portland, OR',
-					date: 'Sun, 2 June 2013'
-				},
-				{ request: true,
-					description: 'Looking for a ride down to LA, on wednesday. Im a student, Headed down for a long weekend. I have one small bag and a bass guitar. Im a Very chill, and easy going guy. Let me know if your headed down and looking for a rider. I got cash in hand, gimme a call / 510 910 8927',
-					from: 'San Francisco, CA',
-					to: 'Los Angeles, CA',
-					date: 'Mon, 3 June 2013'
+					title: "Need ride To Atlanta! Must be there by 4pm Tuesday",
+					description: "I need to go to court in Atlanta and i have to be there by 4pm. I am willing to split gas cost with someone. I dont have a vehicle to drive that far so we would need to drive yours. AA female. Non smoker but i dont mind if you. Please email me asap for more detail!",
+					from: "Chicago, IL",
 				}])
 
 @mloseke_rides = Ride.create([
 				{ request: true,
-					description: "I'm going out to work on TRPN with Keith and Brian",
-					from: 'Chicago, IL',
-					to: 'San Francisco, CA',
-					date: 'Tue, 4 June 2013'
+					title: "Pet Needs A Lift to L.A.!",
+					description: "Hi There!\n\nLooking for a kind & responsible with experience traveling with a pet to give my sweet dog, Roguey Bear, a ride to L.A. She'll probably just sleep, stick her pretty head out the window (if you let her), and wear a goofy grin the whole time. I really miss her and I would love to reunited with her as soon as possible! If you need a co-pilot for your road trip then just let me know and I would be willing to help you with your cross country trek.\n\nPlease let me know at your earliest convenience. I would like her/us to be on the road in the next couple of weeks and we can work out the rest of the details in a follow-up email.\n\nThank you so much for reading and safe travels!",
+					from: "Los Angeles, CA",
 				},
 				{ request: true,
-					description: 'I have a backpack and a soft case guitar. I can chip in to split gas, of course. Thanks!',
-					from: 'Chicago, IL',
-					to: 'New York, NY',
-					date: 'Wed, 5 June 2013'
+					title: "Need Ride to Chicago-- Dwight IL to Chicago",
+					description: "Hello,\nI absolutely need to get into city tonight and I'm broke and know it's a long shot but calling anyone making a trip up I-55 towards the city and would like a little company for the ride up!!\n\nI'm only about an hour south of the city, so if you're willing and super awesome shoot me an email or a text at (###) 555 0886",
+					from: "Dwight, IL",
 				},
 				{ request: true,
-					description: 'Hey yall! need a ride from chicago to Fairfield or somewhere close on (3/26) (mt pleasant, ottomwa, iowa city etc) Gas money, food and laughs provided by me! thanks!',
-					from: 'Chicago, IL',
-					to: 'Fairfield, IA',
-					date: 'Thu, 6 June 2013'
+					title: "Need a Ride to Florida after 6/16",
+					description: "Hi I need a ride to FL to pick up a truck I purchased off the internet. I'm a clean 27 YO Sane Male who doesn't smoke or drink very respectful. Will be gladly willing to split the cost of gas the whole way down. Truck is Located in Verde Beach, FL Near Jacksonville. if anyone is heading down there soon and wants to cut there costs down please feel free to contact me.",
+					from: "Crown Point, IN",
 				}])
 
 @bstolte.rides = @bstolte_rides
