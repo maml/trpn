235ab6d996fa22ad4713bacc4d1c8d8d981ebf35
Matthew Loseke <mloseke@gmail.com>
2013-06-11 12:13:29 -0500

    clean up routes syntax

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,13 @@ Trpn::Application.routes.draw do
 
 	devise_for :users
   
-	# get "splash/index"
-  
 	resources :users, :only => [:index, :show] do
-  	resources :rides, :controller => 'users/rides'
+  	resources :rides, :controller => "users/rides"
 	end
 
 	resources :rides do
-		resources :messages, :controller => 'rides/messages'
+		resources :messages, :controller => "rides/messages"
 	end
-
-	get "rides", controller: "rides", action: "all"
 
 	resources :subscriptions, :only => [:create]
 
