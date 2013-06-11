568ee09676520121d28cce591e3042c5485706f2
Matthew Loseke <mloseke@gmail.com>
2013-06-11 11:21:09 -0500

    add belongs_to user for messages, migration to add user_id to messages table

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,5 @@ class Message < ActiveRecord::Base
   attr_accessible :message
 	validates :message, :presence => true
 	belongs_to :ride
+	belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130609235038) do
+ActiveRecord::Schema.define(:version => 20130611161947) do
 
   create_table "messages", :force => true do |t|
     t.text     "message"
     t.integer  "ride_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "rides", :force => true do |t|
