98fc3f7ae73b13bc5babf8e6f925ad0fecd33cfa
Matthew Loseke <mloseke@gmail.com>
2013-08-12 12:31:05 -0500

    Revert "add ride_id to conversations table"
    This reverts commit f800ad26295a35f718edda363ee9789455b9607c.

--- db/migrate/20130812164759_add_ride_id_to_conversations.rb
+++
@@ -1,5 +0,0 @@
-class AddRideIdToConversations < ActiveRecord::Migration
-  def change
-    add_column :conversations, :ride_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130812164759) do
+ActiveRecord::Schema.define(:version => 20130806181602) do
 
   create_table "conversations", :force => true do |t|
     t.string   "subject",    :default => ""
     t.datetime "created_at",                 :null => false
     t.datetime "updated_at",                 :null => false
-    t.integer  "ride_id"
   end
 
   create_table "messages", :force => true do |t|
