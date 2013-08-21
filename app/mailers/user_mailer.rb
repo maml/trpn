acab9129cdcf657e36039c9d957ff9bc68c58644
Matthew Loseke <mloseke@gmail.com>
2013-08-20 21:36:08 -0500

    pull out Express Pigeon's URI and auth key from app and into ENV variables

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
-require 'net/http'
-require 'json'
+require "net/http"
+require "json"
 
 class UserMailer
 	
 	class << self
 		def launch_welcome_email(user)
-			uri = URI.parse('https://api.expresspigeon.com/messages')
+			uri = URI.parse(ENV["EXPRESS_PIGEON_URI"])
 			req = Net::HTTP::Post.new uri.path
 
 			body = 
@@ -21,9 +21,8 @@ class UserMailer
 			}
 
 			req.body = body.to_json
-			# using sandbox auth key . . .
-			req['X-auth-key'] = '0edc7843-84fb-4e84-a621-9e617597e27d'
-			req['Content-type'] = 'application/json'
+			req["X-auth-key"] = ENV["EXPRESS_PIGEON_AUTH_KEY"] 
+			req["Content-type"] = "application/json"
 
 			res = Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
 				http.request req
