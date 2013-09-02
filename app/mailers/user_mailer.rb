3c891406d7403fd1a0b13d8c19f4e07dc43de0d8
Matthew Loseke <mloseke@gmail.com>
2013-09-01 20:46:11 -0500

    remove duplication in UserMailer methods into a method named, mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,25 +5,18 @@ class UserMailer
 
 	WELCOME_EMAIL_ID = 10084
 	PASSWORD_RESET_ID = 10648
+
+	EXPRESS_PIGEON_URI = URI.parse(ENV["EXPRESS_PIGEON_URI"])
+	REQUEST = Net::HTTP::Post.new UserMailer::EXPRESS_PIGEON_URI.path
 	
 	class << self
 
-		def launch_welcome_email(user)
-			uri = URI.parse(ENV["EXPRESS_PIGEON_URI"])
-			req = Net::HTTP::Post.new uri.path
-
-			body = 
-			{
-				"template_id" => UserMailer::WELCOME_EMAIL_ID,
-				"reply_to"  => "mloseke@gmail.com",
-				"from"  => "Matt Loseke",
-				"to"  => "mloseke@gmail.com",
-				"subject"  => "Welcome to TRPN",
-				"merge_fields"  => {
-					"first_name"  => "#{user.name}"
-				}
-			}
-
+		def mail(user, opts={})
+			uri = UserMailer::EXPRESS_PIGEON_URI
+			req = UserMailer::REQUEST
+			
+			body = opts
+			
 			req.body = body.to_json
 			req["X-auth-key"] = ENV["EXPRESS_PIGEON_AUTH_KEY"] 
 			req["Content-type"] = "application/json"
@@ -33,10 +26,22 @@ class UserMailer
 			end
 		end
 
-		def password_reset(user)
-			uri = URI.parse(ENV["EXPRESS_PIGEON_URI"])
-			req = Net::HTTP::Post.new uri.path
+		def launch_welcome_email(user)
+			body = 
+			{
+				template_id: UserMailer::WELCOME_EMAIL_ID,
+				reply_to: "mloseke@gmail.com",
+				from: "TRPN HQ",
+				to:  "mloseke@gmail.com",
+				subject: "Welcome to TRPN",
+				merge_fields: {
+					first_name: "#{user.name}"
+				}
+			}
+			mail(user, body)
+		end
 
+		def password_reset(user)
 			body = 
 			{
 				"template_id" => UserMailer::PASSWORD_RESET_ID,
@@ -48,14 +53,7 @@ class UserMailer
 					"password_reset_url"  => "#{Trpn::Application.config.mailer_uri}#{Rails.application.routes.url_helpers.edit_password_reset_url(user.password_reset_token, :only_path => true)}"
 				}
 			}
-
-			req.body = body.to_json
-			req["X-auth-key"] = ENV["EXPRESS_PIGEON_AUTH_KEY"] 
-			req["Content-type"] = "application/json"
-
-			res = Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
-				http.request req
-			end
+			mail(user, body)
 		end
 
 	end
